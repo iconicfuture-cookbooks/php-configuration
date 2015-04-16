6f680debc127ecc1e6e41211d1eab9c5385eebad
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-04-16 16:36:23 +0200

    CPB-509 normalize recipe names

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "php-fpm"
+name             "php-configuration"
 maintainer_email "mathias.latzko@iconicfuture.com"
 maintainer       "Mathias Latzko"
 license          "Apache 2.0"
 description      "Configure php for a application."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1"
 
 depends 'php'
 
