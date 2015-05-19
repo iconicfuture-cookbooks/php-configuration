f869436b257d4664cde4c8cf776f3e8027decc36
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-05-19 11:11:19 +0200

    CPB-594 pushed version to 1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       "Mathias Latzko"
 license          "Apache 2.0"
 description      "Configure php for an application."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2"
+version          "1.0"
 
 depends 'php'
 
