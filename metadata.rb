bb1970aa991f7afb0a0aa36fb2804fb599168fb5
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-05-27 11:43:40 +0200

    CPB-605 extend the www fpm pool and push the cb version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "php-configuration"
-maintainer_email "mathias.latzko@iconicfuture.com"
-maintainer       "Mathias Latzko"
+maintainer_email "thomas.liebscher@iconicfuture.com"
+maintainer       "Thomas Liebscher"
 license          "Apache 2.0"
 description      "Configure php for an application."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0"
+version          "1.1.0"
 
 depends 'php'
 
