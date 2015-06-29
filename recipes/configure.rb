726e052de976e7f6f9f1443503d437ec9659f37b
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-06-29 15:56:52 +0200

    CPB-616 turn off apc for php > 5.5

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -33,6 +33,9 @@ template "#{node['php']['ext_conf_dir']}/apc.ini" do
   owner "root"
   group "root"
   mode "0644"
+  only_if do
+    "#{node['php']['packages']}".include? 'php-apc'
+  end
 end
 
 #
